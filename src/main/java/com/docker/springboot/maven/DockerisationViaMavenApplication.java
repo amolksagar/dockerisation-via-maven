7fa16f884fdc3bd6a41aca2d4514d5a7a272fd21
Amol Kshirsagar <kksagar.amol@gmail.com>
2019-03-26 22:01:47 +1100

    Dockerisation via maven

---
+++ src/main/java/com/docker/springboot/maven/DockerisationViaMavenApplication.java
@@ -0,0 +1,13 @@
+package com.docker.springboot.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerisationViaMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerisationViaMavenApplication.class, args);
+	}
+
+}
